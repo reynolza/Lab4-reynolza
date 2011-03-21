28cd0112193d72f421184fd5b6191dfc88e3a62b
Zac Reynolds <reynolza@rose-hulman.edu>
2011-03-21 18:03:03 -0400

    Questions and who.txt for the lab

---
+++ Lab4-AdvancedUnitTesting-Code/Lab4-AdvancedUnitTesting-Code/ExpediaTest/CarTest.cs
@@ -0,0 +1,66 @@
+using System;
+using NUnit.Framework;
+using Expedia;
+using Rhino.Mocks;
+
+namespace ExpediaTest
+{
+	[TestFixture()]
+	public class CarTest
+	{	
+		private Car targetCar;
+		private MockRepository mocks;
+		
+		[SetUp()]
+		public void SetUp()
+		{
+			targetCar = new Car(5);
+			mocks = new MockRepository();
+		}
+
+        [Test()]
+        public void TestThatCarGetsMileageFromDatabase()
+        {
+            IDatabase mockDatabase = mocks.Stub<IDatabase>();
+            int Mileage = 37000;
+            mockDatabase.Miles = Mileage;
+            var target = new Car(10);
+            target.Database = mockDatabase;
+            int mileCount = target.Mileage;
+            Assert.AreEqual(mileCount, Mileage);
+        }
+		
+		[Test()]
+		public void TestThatCarInitializes()
+		{
+			Assert.IsNotNull(targetCar);
+		}	
+		
+		[Test()]
+		public void TestThatCarHasCorrectBasePriceForFiveDays()
+		{
+			Assert.AreEqual(50, targetCar.getBasePrice()	);
+		}
+		
+		[Test()]
+		public void TestThatCarHasCorrectBasePriceForTenDays()
+		{
+            var target = new Car(10);
+			Assert.AreEqual(80, target.getBasePrice());	
+		}
+		
+		[Test()]
+		public void TestThatCarHasCorrectBasePriceForSevenDays()
+		{
+			var target = new Car(7);
+			Assert.AreEqual(10*7*.8, target.getBasePrice());
+		}
+		
+		[Test()]
+		[ExpectedException(typeof(ArgumentOutOfRangeException))]
+		public void TestThatCarThrowsOnBadLength()
+		{
+			new Car(-5);
+		}
+	}
+}
